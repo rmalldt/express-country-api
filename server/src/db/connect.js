acebfdb99d6c526954e4607984e34fe7e80701a3
rmalldt <rmall.dt@gmail.com>
2025-05-18 12:08:11 +0100

    Restructure file for database and server directories

---
+++ server/src/db/connect.js
@@ -0,0 +1,35 @@
+const { Pool } = require('pg');
+require('dotenv').config();
+
+let db;
+
+function setDb() {
+  switch (process.env.NODE_ENV) {
+    case 'development':
+    case 'test':
+      db = new Pool({
+        connectionString: process.env.SUPABASE_URL,
+      });
+      break;
+    case 'production':
+      db = new Pool({
+        user: process.env.DB_USER,
+        host: process.env.DB_HOST,
+        database: process.env.DB_NAME,
+        password: process.env.DB_PASSWORD,
+        port: process.env.DB_PORT || 5432,
+      });
+      break;
+
+    default:
+      console.log('Database connection error', err.stack);
+      return;
+  }
+
+  console.log(`DB connection established for ${process.env.NODE_ENV}`);
+  console.log('DB info: ', db);
+}
+
+setDb();
+
+module.exports = db;
